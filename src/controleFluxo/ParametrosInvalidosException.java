45f8d24c21cbd306f22cc526a5a55fc103d81947
LealJP <juportoleal@gmail.com>
2024-06-23 21:50:56 -0300

    controle de fluxo finalizado

---
+++ src/controleFluxo/ParametrosInvalidosException.java
@@ -0,0 +1,5 @@
+package controleFluxo;
+
+//classe para tratar uma exceção prevista pelo desenvolvedor, essa classe deve extender lógica/tratamento (herdar) da classe Exception
+//extender da classe Exception envolve conceito de Herança
+public class ParametrosInvalidosException extends Exception{}
